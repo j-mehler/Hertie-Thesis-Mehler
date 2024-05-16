259080af4a9f6b2e22ddced311e8d56d97e8f7f7
j-mehler <89975174+j-mehler@users.noreply.github.com>
2024-05-16 18:58:41 +0200

    Add thesis file (pdf)

--- code/0_pre-processing/0_setup.R
+++ code/0_pre-processing/0_setup.R
@@ -1,12 +1,7 @@
 # Packages ----
 
 library(tidyverse)
-#library(naniar)
-#library(ggplot2)
-#library(gridExtra)
-#library(haven)
-#library(knitr)
 
 # Theme ----
 
-theme_set(theme_minimal())
+theme_set(theme_bw())
